05266f23e41725f7836acb28a233305f41a2f3d0
AndreaMargutti <marguttiandrea30@gmail.com>
2025-03-03 17:37:31 +0100

    started hamburger menu

--- components/Header/HeaderHamburger.vue
+++ components/Header/HeaderHamburger.vue
@@ -1,33 +1,62 @@
+<script setup>
+const info = ref([
+    { id: 1, label: 'Chi Siamo' },
+    { id: 2, label: 'Comitato Editoriale' },
+    { id: 3, label: 'Servizi' },
+    { id: 4, label: 'Agenda/Eventi' },
+    { id: 5, label: `Mappa dell'attivismo` },
+    { id: 6, label: 'Inchieste crowdfunding' }
+])
+</script>
+
 <template>
-    <h1>SEZIONE HAMBURGER MENU</h1>
-    <div>
-        <div>
+    <div class="flex flex-wrap">
+        <div class="border">
             <h1>Articoli</h1>
             <p>Trend, dati e novità del Terzo Settore.</p>
         </div>
-        <div>
+        <div class="border">
             <h1>Storie</h1>
             <p>Letture ed approfondimenti dei fenomeni complessi.</p>
         </div>
-        <div>
+        <div class="border">
             <h1>Interviste</h1>
             <p>Racconti reali di persone ed organizzazioni.</p>
         </div>
-        <div>
+        <div class="border">
             <h1>Opinioni</h1>
             <p>Riflessioni e confronti dei nostri opinionisti.</p>
         </div>
-        <div>
+        <div class="border">
             <h1>Podcast</h1>
             <p>Un luogo per dare voce ai protagonisti della sostenibilità.</p>
         </div>
-        <div>
+        <div class="border">
             <h1>Bookazine</h1>
             <p>Una rivista da leggere e un libro da conservare.</p>
         </div>
     </div>
+    <div>
+        <HeaderHamburgerFooter :info="info"/>
+    </div>
 </template>
 
 <style scoped lang="scss">
+    h1 {
+        font-family: 'Raleway';
+        font-weight: 700;
+        font-size: 48px;
+    }
 
+    .flex {
+        div {
+            flex-basis: calc(100% / 3);
+            padding: 40px;
+            padding-top: 144px;
+        }
+
+        p {
+            font-size: 14px;
+        }
+    }
 </style>
---
+++ components/Header/HeaderHamburgerFooter.vue
@@ -0,0 +1,28 @@
+<script setup>
+const props = defineProps({
+    info: Array
+})
+
+</script>
+
+<template>
+    <ul class="flex">
+        <li v-for="{ id, label } in info" :key="id" class="text-center">
+            <p>{{ label }}</p>
+        </li>
+    </ul>
+</template>
+
+<style scoped lang="scss">
+ li {
+    padding: 65px 0;
+    flex-basis: calc(100% / 6);
+    align-content: stretch;
+    border-right: 1px solid black;
+
+    p {
+        font-size: 14px;
+        font-weight: 400;
+    }
+ }
+</style>
