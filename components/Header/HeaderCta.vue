ef83124df04a9011e7b752035f8312b9dcfb137e
AndreaMargutti <marguttiandrea30@gmail.com>
2025-03-03 12:21:47 +0100

    started HeaderCta

--- components/Header/HeaderCta.vue
+++ components/Header/HeaderCta.vue
@@ -1,7 +1,56 @@
 <template>
-    <h1>Cta</h1>
+    <div class="flex text-center mt-1">
+        <div class="flex-basis-50 border border--right--0 py-1">
+            Contribuisci
+        </div>
+        <div class="flex-basis-50 border py-1">
+            Abbonati
+        </div>
+    </div>
 </template>
 
-<style scoped lang="scss">
+<style scoped>
+
+    /* margin */
+
+    .mt-1 {
+        margin-top: 30px;
+    }
+
+    /* flex */
+
+    .flex {
+        display: flex;
+    }
+
+    .flex-basis-50 {
+        flex-basis: 50%;
+    }
+
+    .text-center {
+        text-align: center;
+    }
+
+    /* padding */
+
+    .py-1 {
+        padding: 5px;
+    }
+
+    /* bordi */
+
+    .border {
+        border: 0.5px solid black
+    }
+
+    .border--right--0 {
+        border-right: 0;
+    }
+
+    /* white-bar */
+
+    #white-bar {
+        height: 20px;
+    }
 
 </style>
