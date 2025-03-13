d9fdee18bd3c80680949d984d9bec59b4c6fcae1
AndreaMargutti <marguttiandrea30@gmail.com>
2025-03-13 15:11:36 +0100

    [refactor] HamburgerFooter: better css classes

--- components/Header/HamburgerFooter.vue
+++ components/Header/HamburgerFooter.vue
@@ -1,45 +1,42 @@
 <script setup lang="ts">
 const props = defineProps({
-  info: Array,
+  info: { type: Array, required: true }
 });
 </script>
 
 <template>
-  <ul class="d-flex flex-column flex-row">
-    <li v-for="{ id, label } in info" :key="id" class="text-center">
-      <p>{{ label }}</p>
+  <ul class="d-flex hamburger-footer">
+    <li v-for="{ id, label } in info" :key="id" class="hamburger-footer__item text-center">
+      <p class="hamburger-footer__text">{{ label }}</p>
     </li>
   </ul>
 </template>
 
 <style scoped lang="scss">
-.flex-column {
+
+.hamburger-footer {
   flex-direction: column;
-}
-
-ul {
   height: 100%;
-}
 
-li {
+  @include for-tablet() {
+    flex-direction: row;
+  }
+
+  &__item {
   padding: 65px 0;
   flex-basis: calc(100% / 6);
   align-content: stretch;
   border-bottom: 1px solid black;
-  p {
+
+    @include for-desktop () {
+      border-bottom: 0;
+      border-right: 1px solid black;
+    }
+  }
+
+  &__text {
     font-size: 14px;
     font-weight: 400;
-  }
-}
-
-@media screen and (min-width: 970px) {
-  .flex-row {
-    flex-direction: row;
-  }
-
-  li {
-    border-bottom: 0;
-    border-right: 1px solid black;
   }
 }
 </style>
