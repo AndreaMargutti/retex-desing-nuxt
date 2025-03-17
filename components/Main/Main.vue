60cd4ece878edf4a45a908d2e0e766eddcba305e
AndreaMargutti <marguttiandrea30@gmail.com>
2025-03-17 10:43:00 +0100

    spliced middle article

--- components/Main/Main.vue
+++ components/Main/Main.vue
@@ -9,13 +9,21 @@ const articles = data?.value?.articles ?? [];
 console.log(articles);
 
 const topArticles = articles.toSpliced(4);
-console.log(topArticles);
+console.log("top", topArticles);
 console.log(articles);
+
+const middleArticles = () => {
+  const firstSplice = articles.toSpliced(0, 4);
+  return firstSplice.toSpliced(5);
+};
+
+console.log(middleArticles());
 </script>
 
 <template>
   <MainTopArticles :articles="topArticles" />
   <MainContributeBanner />
+  <MainMiddleArticles :articles="middleArticles()" />
   <MainCitationBanner />
   <MainBlog />
 </template>
---
+++ components/Main/MiddleArticles.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const props = defineProps({
+  articles: { type: Array, required: true },
+});
+</script>
+
+<template>
+  <ul>
+    <li v-for="article in articles" :key="article.id">
+      {{ article.title }}
+    </li>
+  </ul>
+</template>
